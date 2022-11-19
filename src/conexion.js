e8116066e6f6ef2ca221bc57731ca5845a343af7
RodolfoE <j.estra71@gmail.com>
2022-11-18 22:25:27 -0600

    azure db

--- src/conexion.js
+++ src/conexion.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql')
 const conexion = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    host: process.env.DB_HOST || 'sqloserver.mysql.database.azure.com',
+    user: process.env.DB_USER || 'maeqadmin@sqloserver',
+    password: process.env.DB_PASSWORD || 'm4eqbdpa$$w0rd',
     port: process.env.DB_PORT || 3306,
     database: process.env.DB_NAME || 'constructora'
 });
